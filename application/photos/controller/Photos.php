9e332b8629ff0f05254a64709e02df7f7b3061e1
code2here <554436987@qq.com>
2021-12-01 14:16:48 +0800

    上传图片接口

--- application/photos/controller/Photos.php
+++ application/photos/controller/Photos.php
@@ -15,6 +15,7 @@ class Photos {
             // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
             echo $info->getSaveName();
             // 输出 42a79759f284b767dfcb2a0197904287.jpg
+
             echo $info->getFilename();
         }else{
             // 上传失败获取错误信息
